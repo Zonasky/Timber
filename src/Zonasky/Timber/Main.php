166b4b2a2bf3a7f9e8048b61cdde55735af1a627
Inaayat <125380942+Inaay@users.noreply.github.com>
2023-04-10 23:16:52 -0700

    Update Main.php

--- src/Zonasky/Timber/Main.php
+++ src/Zonasky/Timber/Main.php
@@ -40,18 +40,26 @@ class Main extends PluginBase implements Listener {
         }
         if ($block->getId() == BlockLegacyIds::LOG || $block->getId() == BlockLegacyIds::LOG2) {
             $treeBlocks = $this->getTreeBlocks($block);
-            $level = $block->getPosition()->getWorld();
+            $world = $block->getPosition()->getWorld();
+            $leaves = [];
             foreach ($treeBlocks as $treeBlock) {
-                $level->useBreakOn($treeBlock->getPosition());
+                $world->useBreakOn($treeBlock->getPosition());
+                $leaves = array_merge($leaves, $this->getLeavesBlocks($treeBlock));
+            }
+            $leavesConfig = $this->getConfig()->get("leaves");
+            if ($leavesConfig) {
+                foreach ($leaves as $leaf) {
+                    $world->useBreakOn($leaf->getPosition());
+                }
             }
         }
     }
 
 	private function getTreeBlocks(Block $block): array {
-        $level = $block->getPosition()->getWorld();
+        $world = $block->getPosition()->getWorld();
         $blocks = [$block];
-        for ($y = $block->getPosition()->getY() + 1; $y <= $level->getMaxY(); $y++) {
-            $blockAbove = $level->getBlock(new Vector3($block->getPosition()->getX(), $y, $block->getPosition()->getZ()));
+        for ($y = $block->getPosition()->getY() + 1; $y <= $world->getMaxY(); $y++) {
+            $blockAbove = $world->getBlock(new Vector3($block->getPosition()->getX(), $y, $block->getPosition()->getZ()));
             if ($blockAbove->getId() == BlockLegacyIds::LOG || $block->getId() == BlockLegacyIds::LOG2) {
                 $blocks[] = $blockAbove;
             } else {
@@ -59,7 +67,7 @@ class Main extends PluginBase implements Listener {
             }
         }
         for ($y = $block->getPosition()->getY() - 1; $y >= 0; $y--) {
-            $blockBelow = $level->getBlock(new Vector3($block->getPosition()->getX(), $y, $block->getPosition()->getZ()));
+            $blockBelow = $world->getBlock(new Vector3($block->getPosition()->getX(), $y, $block->getPosition()->getZ()));
             if ($blockBelow->getId() == BlockLegacyIds::LOG || $block->getId() == BlockLegacyIds::LOG2) {
                 $blocks[] = $blockBelow;
             } else {
@@ -68,4 +76,30 @@ class Main extends PluginBase implements Listener {
         }
         return $blocks;
     }
+
+    private function getLeavesBlocks(Block $block): array {
+		$world = $block->getPosition()->getWorld();
+		$blocks = [];
+		$visited = [];
+		$queue = [$block];
+		while (!empty($queue)) {
+			$current = array_shift($queue);
+			if (in_array($current, $visited)) {
+				continue;
+			}
+			$visited[] = $current;
+			for ($x = $current->getPosition()->getX() - 1; $x <= $current->getPosition()->getX() + 1; $x++) {
+				for ($y = $current->getPosition()->getY() - 1; $y <= $current->getPosition()->getY() + 1; $y++) {
+					for ($z = $current->getPosition()->getZ() - 1; $z <= $current->getPosition()->getZ() + 1; $z++) {
+						$leaf = $world->getBlock(new Vector3($x, $y, $z));
+						if ($leaf->getId() == BlockLegacyIds::LEAVES || $leaf->getId() == BlockLegacyIds::LEAVES2) {
+							$blocks[] = $leaf;
+							$queue[] = $leaf;
+						}
+					}
+				}
+			}
+		}
+		return $blocks;
+	}
 }
